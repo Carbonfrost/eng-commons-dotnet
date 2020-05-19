36dd980eb22fce2a4ec545d2496cfebc9fabbe7b
Gary Mann <garythemann@users.noreply.github.com>
2020-05-18 22:22:14 -0700

    Initial support for an installer

--- eng/.mk/eng.mk
+++ eng/.mk/eng.mk
@@ -1,7 +1,18 @@
 #
 # Various commands for the Engineering platform itself
 #
-.PHONY: release/requirements eng/check eng/update
+
+# Because these are used for installing eng itself, we can't depend on any of the utility targets
+# or variables, so they are redefined here
+
+.PHONY: \
+	eng/check \
+	eng/install \
+	eng/update \
+	release/requirements \
+
+## Get started with eng, meant to be used in a new repo
+eng/start: eng/update -eng/start-Makefile
 
 release/requirements:
 	$(Q) eng/release_requirements
@@ -11,17 +22,37 @@ eng/check:
 
 eng_update_file:=$(shell mktemp)
 
-eng/update: -download-eng-archive -check-command-curl
-	$(Q) (tar -xvf "$(eng_update_file)" --strip-components=1 'eng-commons-dotnet-master/eng/*'; \
-		tar -xvf "$(eng_update_file)" --strip-components=2 'eng-commons-dotnet-master/integration/*'; \
+eng/update: -download-eng-archive
+	$(Q) (tar -xf "$(eng_update_file)" --strip-components=1 'eng-commons-dotnet-master/eng/*'; \
+		tar -xf "$(eng_update_file)" --strip-components=2 'eng-commons-dotnet-master/integration/*'; \
 	)
 
 ifeq ($(ENG_DEV_UPDATE), 1)
--download-eng-archive:
+-download-eng-archive: -check-eng-updates-requirements
 	$(Q) git archive --format=zip --prefix=eng-commons-dotnet-master/ --remote=file://$(HOME)/source/eng-commons-dotnet master -o $(eng_update_file)
 
 else
--download-eng-archive: -check-command-curl
+-download-eng-archive: -check-eng-updates-requirements
 	$(Q) curl -o "$(eng_update_file)" -sL https://github.com/Carbonfrost/eng-commons-dotnet/archive/master.zip
-
 endif
+
+
+_RED = \x1b[31m
+_RESET = \x1b[39m
+
+-check-eng-updates-requirements:
+	@ if [ ! $(shell command -v curl ) ]; then \
+		echo "$(_RED)fatal: $(_RESET)must have curl to download files"; \
+		exit 1; \
+	fi
+	@ if [ ! $(shell command -v tar ) ]; then \
+		echo "$(_RED)fatal: $(_RESET)must have tar to unarchive files"; \
+		exit 1; \
+	fi
+	@ if [ ! $(shell command -v git ) ]; then \
+		echo "$(_RED)fatal: $(_RESET)must have git to unarchive files"; \
+		exit 1; \
+	fi
+
+-eng/start-Makefile:
+	$(Q) printf -- "-include eng/.mk/*.mk\nstart:\n\t@ echo 'The Future awaits !'" > Makefile
