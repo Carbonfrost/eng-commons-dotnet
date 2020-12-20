345e00da8651fd9f42db2c74b60d7660a9160f15
Gary Mann <garythemann@users.noreply.github.com>
2020-12-20 13:36:21 -0800

    Various updates for help; restructure entry point
    * Richer help screen when GNU awk is available
    * Add checks and doctor
    * Remove extra space between empty group descriptions

--- eng/.mk/help.mk
+++ eng/.mk/help.mk
@@ -1,22 +1,48 @@
-_DISABLED_RUNTIMES = $(subst $(_SPACE),$(_PIPE),$(strip $(ENG_DISABLED_RUNTIMES)))
-
 ifdef ALL
-	_FILTER_DISABLED_TARGETS = | grep --color -E '(zzz|$(_DISABLED_RUNTIMES))/|$$' -
+	_HELP_MAKEFILE_LIST = $(MAKEFILE_LIST)
 else
-	_FILTER_DISABLED_TARGETS = | grep -vE '(zzz|$(_DISABLED_RUNTIMES))/' -
+	_DISABLED_RUNTIMES = $(subst $(_SPACE),$(_PIPE),$(strip $(ENG_DISABLED_RUNTIMES)))
+	_HELP_MAKEFILE_LIST = $(shell echo "$(MAKEFILE_LIST)" | sed -E "s/[^ ]+($(_DISABLED_RUNTIMES)).mk//g")
 endif
+
+.PHONY: help \
+	list \
+	doctor \
+
+_AWK_VERSION = $(shell awk --version)
+
+# Show help when no other goal is specified
+.DEFAULT_GOAL = help
 
 ## Show this help screen
 help:
-	@ awk '/^[^-][/a-zA-Z\-\_0-9]+:/ { \
-		helpMessage = match(lastLine, /(.+)/); \
-		if (helpMessage) { \
-			helpCommand = substr($$1, 0, index($$1, ":")-1); \
-			helpMessage = lastLine; \
-			gsub(/##/, " ", helpMessage); \
-			printf "  $(_CYAN)%-16s$(_RESET) %s\n", helpCommand, helpMessage; \
-			lastLine = ""; \
-		} \
-	} \
-	{ if (match($$0, /^##/)) { lastLine = lastLine $$0 } }' $(MAKEFILE_LIST) | \
-		sort $(_FILTER_DISABLED_TARGETS)
+	@ if [[ "$(_AWK_VERSION)" == *"GNU Awk"* ]]; then \
+		awk -f $(_ENG_MAKEFILE_DIR)/.mk/awk/makefile-help-screen.awk $(_HELP_MAKEFILE_LIST); \
+	else \
+		awk -f $(_ENG_MAKEFILE_DIR)/.mk/awk/makefile-simple-help-screen.awk $(_HELP_MAKEFILE_LIST) | sort; \
+	fi
+
+## List all targets
+list:
+	@ awk -f $(_ENG_MAKEFILE_DIR)/.mk/awk/makefile-list-targets.awk $(MAKEFILE_LIST) | grep -vE '^\.' | uniq | sort
+
+## Diagnose common issues
+doctor: \
+	-checks \
+	-preflight-checks \
+
+# "slow" checks used by doctor
+-checks:
+
+# "fast" checks that are implied on any nacho command
+-preflight-checks:
+
+# macOS-specific checks (or not)
+-darwin-preflight-checks:
+-non-darwin-preflight-checks:
+
+ifeq ($(UNAME),Darwin)
+-preflight-checks: -darwin-preflight-checks
+else
+-preflight-checks: -non-darwin-preflight-checks
+endif
